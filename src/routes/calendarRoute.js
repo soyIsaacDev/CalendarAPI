066be610727e4f2b3e8607bf37270f86821fdbfb
Isaac Borbon <borbonisaac@hotmail.com>
2022-09-10 14:56:06 -0700

    Validacion de nuevos eventos entre fechas de eventos pasados

--- src/routes/calendarRoute.js
+++ src/routes/calendarRoute.js
@@ -1,28 +1,57 @@
 const server = require("express").Router();
+const { Op } = require("sequelize");
 
 const { Staff, CalendarEvents } = require("../db");
 
 server.post("/nuevoEvento", async (req, res) => { 
     try {
-      const { kind, start, end, colorId, staffId} = req.body;
-      const event = await CalendarEvents.findOrCreate({
-          where: {
-            start
-          },
-          defaults:{
-            kind, end, colorId,StaffId:staffId
-          }
-      });
-      /* const staff = await Staff.findOne({
-        where:{
-          id: staffId
-        }
-      })
-      console.log(staff)
-      event.addStaff(staff); */
+      const { kind, eventStart, eventEnd, colorId, IdStaff} = req.body;
+      // Sequelize arroja error si no se define los eventos como Date despues de recibirlos
+      const eventStartDate = new Date(eventStart);
+      const eventEndDate = new Date(eventEnd);
 
-      console.log(event)
-      res.json(event);
+      const storedEvent = await CalendarEvents.findOne({
+        where:{
+          [Op.or]:[
+            // Falta agregarle un ranto a estas fechas para validar nuevos eventos entre fechas
+            {start: eventStartDate},
+            {end: eventEndDate}
+          ]
+        }
+      });
+      
+      if (storedEvent === null){
+        console.log("Estoy en null")
+        const event = await CalendarEvents.create({
+            kind,
+            start: eventStartDate,
+            end: eventEndDate, 
+            colorId,
+            StaffId:IdStaff
+        })
+        res.json(event);
+        } 
+      else if(eventStartDate >= storedEvent.start && eventStartDate <= storedEvent.end || 
+              eventEndDate >= storedEvent.start && eventEndDate <= storedEvent.end){
+          console.log("NO puedo agendar");
+          res.json({respuesta:"no puedo agendar"});
+      } 
+      else {
+        const event = await CalendarEvents.create({
+          kind,
+          start: eventStart,
+          end: eventEnd, 
+          colorId,
+          StaffId:IdStaff
+        })
+        res.json(event);
+        
+      }
+
+      
+     
+
+      
     } catch (error) {
       res.send(error);
     }
