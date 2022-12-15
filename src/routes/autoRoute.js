bb054f5630dd1759634eb0692a44da9ae64fdbcc
soyIsaacDev <86566954+soyIsaacDev@users.noreply.github.com>
2022-12-14 20:49:25 -0700

    Registro Autos segun ultimo

--- src/routes/autoRoute.js
+++ src/routes/autoRoute.js
@@ -11,7 +11,7 @@ const { Cliente, Auto } = require("../db");
 */
 server.post("/nuevo", async (req, res) => { 
   try {
-    const {  ClienteId, Tam, Nombre } = req.body;
+    const {  ClienteId, Tam } = req.body;
 
     const auto = await Auto.findAll({ 
       where: {
@@ -19,20 +19,45 @@ server.post("/nuevo", async (req, res) => {
       }   
     });
 
-    if(auto.length===0){
-      if(auto.Nombre!==Nombre){
-        const crearauto = await Auto.create({
-          ClienteId,
-          Nombre,
-          Tamaño:Tam,
-        })
-        res.send(crearauto);
-      }
+    var Name = null;
+    const ultimoauto = auto.length-1;
+    switch (ultimoauto) {
+      case -1:
+        Name = "Primero"
+        break;
+      
+      case 0:
+        Name = "Segundo"
+        break;
+      case 1:
+        Name = "Tercero"
+        break;
+      case 2:
+        Name = "Cuarto"
+        break;
+      case 3:
+        Name = "Quinto"
+        break;
+      case 4:
+        Name = "Sexto"
+        break;
+      case 5:
+        Name = "Septimo"
+        break;
+      case 6:
+        Name = "Octavo"
+        break;  
+      default:
+        break;
     }
+
+    const crearauto = await Auto.create({
+      ClienteId,
+      Nombre:Name,
+      Tamaño:Tam,
+    })
+    res.send(crearauto);
     
-    else {
-      res.send("El auto ya se encuentra creado")
-    }
   } catch (error) {
     res.send(error);
   }
@@ -58,6 +83,20 @@ server.get("/porcliente/:ClienteId/:Nombre", async (req, res) => {
       }   
     });
     res.json(auto===[]? auto : "Error en el cliente o el nombre del auto");
+  } catch (error) {
+    res.send(error);
+  }
+});
+
+server.get("/porcliente/:ClienteId", async (req, res) => { 
+  try {
+    let { ClienteId } = req.params;
+    const auto = await Auto.findAll({ 
+      where: {
+        ClienteId
+      }   
+    });
+    res.json(auto===[]? auto : "El Cliente no tiene autos registados");
   } catch (error) {
     res.send(error);
   }
